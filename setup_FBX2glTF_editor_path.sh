bb3efd61ed919c4c5362ff26b1c5103d6a2995f2
Terrence Drumm <terrence.drumm@gmail.com>
2023-10-05 10:11:47 -0400

    path

--- setup_FBX2glTF_editor_path.sh
+++ setup_FBX2glTF_editor_path.sh
@@ -7,7 +7,7 @@ echo "EDITOR SETTINGS BEFORE ---------------------------------"
 cat /root/.config/godot/editor_settings-4.tres
 echo "-------------------------------------------------------"
 
-echo "filesystem/import/fbx/fbx2gltf_path = \"/usr/local/bin/FBX2glTF/FBX2glTF-linux-x86_64\"" >> /root/.config/godot/editor_settings-4.tres
+echo "filesystem/import/fbx/fbx2gltf_path = \"/usr/local/bin/FBX2glTF\"" >> /root/.config/godot/editor_settings-4.tres
 
 echo "EDITOR SETTINGS AFTER ---------------------------------"
 cat /root/.config/godot/editor_settings-4.tres
